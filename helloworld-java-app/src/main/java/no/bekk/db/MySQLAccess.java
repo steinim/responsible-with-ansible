5aef461b58f3347219fea8c48ee59fa626198f83
Stein Inge Morisbak <steinim@gmail.com>
2014-02-10 21:39:56 +0100

    It works!

--- helloworld-java-app/src/main/java/no/bekk/db/MySQLAccess.java
+++ helloworld-java-app/src/main/java/no/bekk/db/MySQLAccess.java
@@ -3,49 +3,69 @@ package no.bekk.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MySQLAccess {
-  private Connection connect = null;
-  private Statement statement = null;
-  private ResultSet resultSet = null;
+	private Connection connect = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
+	private String user = null;
+	private String password = null;
+	private String dbServer = null;
+	private String tablename = null;
+	private String schema = null;
+	
 
-  public String sayHello() throws Exception {
-    try {
-      Class.forName("com.mysql.jdbc.Driver");
-      connect = DriverManager.getConnection("jdbc:mysql://db.local/devops?user=devops&password=devops123");
-      statement = connect.createStatement();
-      resultSet = statement.executeQuery("select * from hello");
-      String message = null;
-      while (resultSet.next()) {
-        message = resultSet.getString("MESSAGE");
-        System.out.println("Retrieved " + message + " from database.");
-      }
-      return message;
-    } catch (Exception e) {
-      throw e;
-    } finally {
-      close();
-    }
+	public MySQLAccess() {
+		user = System.getProperty("db.user");
+		schema = System.getProperty("db.schema");
+		password = System.getProperty("db.password");
+		dbServer = System.getProperty("db.server");
+		tablename = System.getProperty("db.tablename");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String dbUrl = "jdbc:mysql://" + dbServer +"/" + schema + "?user="+ user + "&password=" + password;
+			System.out.println("Connecting to database: " + dbUrl);
+			connect = DriverManager.getConnection(dbUrl);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-  }
+	public String sayHello() throws Exception {
+		try {
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("select * from " + tablename);
+			String message = null;
+			while (resultSet.next()) {
+				message = resultSet.getString("MESSAGE");
+			}
+			return message + " :)";
+		} catch (Exception e) {
+			return "nothing :(";
+		} finally {
+			close();
+		}
 
-  private void close() {
-    try {
-      if (resultSet != null) {
-        resultSet.close();
-      }
+	}
 
-      if (statement != null) {
-        statement.close();
-      }
+	private void close() {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
 
-      if (connect != null) {
-        connect.close();
-      }
-    } catch (Exception e) {
+			if (statement != null) {
+				statement.close();
+			}
 
-    }
-  }
+			if (connect != null) {
+				connect.close();
+			}
+		} catch (Exception e) {
 
-} 
+		}
+	}
+
+}
--- helloworld-java-app/src/main/java/no/bekk/jersey/resources/RootResource.java
+++ helloworld-java-app/src/main/java/no/bekk/jersey/resources/RootResource.java
@@ -2,6 +2,8 @@ package no.bekk.jersey.resources;
 
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
+import java.net.InetAddress;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -21,7 +23,7 @@ public class RootResource {
 	@GET
 	@Produces(TEXT_PLAIN)
 	public Response ping() throws Exception {
-		return Response.ok("I'm alive :) " + dbAccess.sayHello()).build();
+		return Response.ok("App says: I'm alive on "+ InetAddress.getLocalHost().getHostName() +" :)\n\nDB says: " + dbAccess.sayHello()).build();
 	}
 
 }
